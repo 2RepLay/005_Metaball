ed183df3a89a57b0d8606886feaae0c971513e2a
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-09-22 01:31:04 +0300

    Initial Commit

---
+++ 005_Metaball/_05_MetaballApp.swift
@@ -0,0 +1,17 @@
+//
+//  _05_MetaballApp.swift
+//  005_Metaball
+//
+//  Created by nikita on 22.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct _05_MetaballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
